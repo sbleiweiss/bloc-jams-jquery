883fce224123802a0db9e1a46e0f1a1197286f95
Sarah <sarah.bleiweiss@gmail.com>
2019-04-26 07:32:16 -0700

    Assignment bloc jams intro album artist & release info mapped

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,6 @@
 {
   $('#album-title').text(album.title); // inserts album title into #album-title element
   $('img#album-cover-art').attr('src', album.albumArtUrl); //shows album cover art
+  $('.artist').text(album.artist); //shows artist name
+  $('#release-info').text(album.releaseInfo); //shows release info
 }
